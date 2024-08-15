28eeb506342d99faa25eaf2510188cd434bd045c
flomok <88573497+flomok@users.noreply.github.com>
2024-08-15 22:05:27 +0800

    Update common.py

--- src/llamafactory/api/common.py
+++ src/llamafactory/api/common.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# 处理基于 Pydantic 模型的数据，将其转换为 Python 字典或 JSON 字符串格式。代码中定义了两个函数 dictify 和 jsonify，它们分别用于将 Pydantic 模型实例转换为字典和 JSON 字符串。
+# 这两个函数都考虑到了 Pydantic 的不同版本（v1 和 v2）可能带来的 API 差异，因此在代码中使用了异常处理机制来兼容这两种版本。
 
 import json
 from typing import TYPE_CHECKING, Any, Dict
